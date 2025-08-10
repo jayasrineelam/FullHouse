45bf3475f31c0ad8e3bb738baec571e74112e726
jayasrineelam <jayasri.neelam@gmail.com>
2025-08-10 16:39:30 -0700

    Support for Continuous prompting feature

--- MovieCalculator.java
+++ MovieCalculator.java
@@ -13,27 +13,41 @@ import java.util.Scanner;
 public class MovieCalculator {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of people");
-        int NumberofPeople = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter the movie number (1 to 4):");
-        int MovieName = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Are you a member? Yes/No: ");
-        String isMemberInput = scanner.nextLine();
-        boolean isMember = isMemberInput.equalsIgnoreCase("Yes");
-        scanner.close();
 
-        int MoviePrice = FindMoviePrice(MovieName);
-        double TotalBillAmount = CalculateBillAmount(NumberofPeople, MoviePrice);
-        TotalBillAmount = AddDiscountBasedOnNumberOfPeople(TotalBillAmount, NumberofPeople);
-        TotalBillAmount = AddDiscountBasedOnMembership(TotalBillAmount, isMember);
-        TotalBillAmount = AddSalestax(TotalBillAmount);
+        for (;;) {           
 
-        System.out.println("The total bill amount with salestax " + TotalBillAmount);
+            System.out.println("Enter the number of people");
+            int NumberofPeople = scanner.nextInt();
+            scanner.nextLine();
+            
+            System.out.println("Enter the movie number (1 to 4):");
+            int MovieName = scanner.nextInt();
+            scanner.nextLine();
+            
+            System.out.println("Are you a member? Yes/No: ");
+            String isMemberInput = scanner.nextLine();
+            boolean isMember = isMemberInput.equalsIgnoreCase("Yes");
+
+            int MoviePrice = FindMoviePrice(MovieName);
+            double TotalBillAmount = CalculateBillAmount(NumberofPeople, MoviePrice);
+            TotalBillAmount = AddDiscountBasedOnNumberOfPeople(TotalBillAmount, NumberofPeople);
+            TotalBillAmount = AddDiscountBasedOnMembership(TotalBillAmount, isMember);
+            TotalBillAmount = AddSalestax(TotalBillAmount);
+
+            System.out.println("The total bill amount with salestax " + TotalBillAmount);
+            System.out.println (" ");
+            System.out.println ("===============NEW BOOKING======================= ");
+
+
+            System.out.println("Do you want to continue? Yes/No");
+            if (!scanner.nextLine().equalsIgnoreCase("Yes")) {
+                scanner.close(); // Close once after loop ends      
+                break; // Exit loop if user says No
+            }
+        }
     }
+
 
     public static int FindMoviePrice(int MovieName) {
 
@@ -63,15 +77,15 @@ public class MovieCalculator {
         return TotalBillAmount;
     }
 
-    public static double AddDiscountBasedOnMembership(double TotalBillAmount,boolean isMember) {
+    public static double AddDiscountBasedOnMembership(double TotalBillAmount, boolean isMember) {
         if (isMember) {
-            return TotalBillAmount *= 0.15;
+            return TotalBillAmount *= 0.85;
         }
-        return TotalBillAmount *= 0.15;
+        return TotalBillAmount;
     }
 
     public static double AddSalestax(double TotalBillAmount) {
-        return TotalBillAmount *= 0.85;
+        return TotalBillAmount *= 1.09;
     }
 
 }
